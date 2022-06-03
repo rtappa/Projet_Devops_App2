16542f564982b91d6a9909499ab7181e9f3cbaf4
rtappa <104068471+rtappa@users.noreply.github.com>
2022-06-03 14:36:39 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from projet.handlers.routes import configure_routes
+from handlers.routes import configure_routes
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
